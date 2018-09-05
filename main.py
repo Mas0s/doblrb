2186a997e92d8fe75d40dd5f559deb826df21641
masos <masos@masos.viewdns.net>
2018-09-05 21:35:32 +0300

    added putpoint command, various improvements

--- main.py
+++ main.py
@@ -3,38 +3,62 @@
 import discord
 import modules.commands as com
 import asyncio
-import aiohttp
-import platform
+from shlex import split as shplit
+from platform import python_version
 
 client = discord.Client()
 
 dmid = '260146161975820288'
 
+man = {
+    'ping': 'Syntax: `*ping`',
+    'getstats': 'Syntax: `*getstats`',
+    'mkchar': 'Syntax: `*mkchar`',
+    'man': 'Syntax: `*man <command>`',
+    'putpoint': 'Syntax: `*putpoint <stat> [number]`'
+}
+
 @client.event
 async def on_ready():
-	print('Logged in as '+client.user.name+' (ID:'+client.user.id+') | Connected to '+str(len(client.servers))+' servers | Connected to '+str(len(set(client.get_all_members())))+' users')
-	print('--------')
-	print('Current Discord.py Version: {} | Current Python Version: {}'.format(discord.__version__, platform.python_version()))
-	print('--------')
-	print('Use this link to invite {}:'.format(client.user.name))
-	print('https://discordapp.com/oauth2/authorize?client_id={}&scope=bot&permissions=8'.format(client.user.id))
-	print('--------')
-	return await client.change_presence(game=discord.Game(name='w/ code'))
+    print('Logged in as '+client.user.name+' (ID:'+client.user.id+') | Connected to '+str(len(client.servers))+' servers | Connected to '+str(len(set(client.get_all_members())))+' users')
+    print('--------')
+    print('Current Discord.py Version: {} | Current Python Version: {}'.format(discord.__version__, python_version()))
+    print('--------')
+    print('Use this link to invite {}:'.format(client.user.name))
+    print('https://discordapp.com/oauth2/authorize?client_id={}&scope=bot&permissions=8'.format(client.user.id))
+    print('--------')
+    await client.change_presence(game=discord.Game(name='w/ code'))
 
 @client.event
 async def on_message(message):
-    if message.content == '*ping':
-        await client.send_message(message.channel, com.ping())
-    elif message.content.startswith('*getstats'):
-        if len(message.content) == 9:
-            await client.send_message(message.channel, com.getstats(message.author.id))
-        elif message.author.id == dmid:
-            await client.send_message(message.channel, com.getstats(shlex.split(message.content)[1]))
+    if message.content.startswith('*ping'):
+        if len(shplit(message.content)) == 1:
+            await client.send_message(message.channel, com.ping())
         else:
-            await client.send_message(message.channel, 'Недостаточно прав!')
-    elif message.content == '*mkchar':
-        await client.send_message(message.channel, com.mkchar(message.author.id))
-
+            await client.send_message(message.channel, man['ping'])
+    elif message.content.startswith('*getstats'):
+        if len(shplit(message.content)) == 1:
+            await client.send_message(message.channel, com.getstats(message.author.id))
+        elif len(shplit(message.content)) == 2:
+            if message.author.id == dmid:
+                await client.send_message(message.channel, com.getstats(shplit(message.content)[1]))
+            else:
+                await client.send_message(message.channel, 'Недостаточно прав!')
+        else:
+            await client.send_message(message.channel, man['getstats'])
+    elif message.content.startswith('*mkchar'):
+        if len(shplit(message.content)) == 1:
+            await client.send_message(message.channel, com.mkchar(message.author.id))
+        else:
+            await client.send_message(message.channel, man['mkchar'])
+    elif message.content.startswith('*putpoint'):
+        if len(shplit(message.content)) == 2:
+            await client.send_message(message.channel, com.putpoint(message.author.id, shplit(message.content)[1]))
+        elif len(shplit(message.content)) == 3:
+            await client.send_message(message.channel, com.putpoint(message.author.id, shplit(message.content)[1], int(shplit(message.content)[2])))
+        else:
+            await client.send_message(message.channel, man['putpoint'])
+    # TODO: man command
 with open('token.txt') as f:
     token = f.read()
 token = token[:-1]
--- modules/commands.py
+++ modules/commands.py
@@ -1,5 +1,6 @@
 import sqlite3
 
+statList = ['strength','dexterity','intelligence']
 #class plr():
 #    def __init__(self):
 #        self.level = 0
@@ -16,30 +17,61 @@ def ping():
 def mkchar(plid):
     data = sqlite3.connect('database.db')
     c = data.cursor()
-    # id, level, str, dex, int, charpts, maxhp, hp
+    # id, level, strength, dexterity, intelligence, charpts, maxhp, hp
+    # CREATE TABLE players (id text, level integer, strength integer, dexterity integer, intelligence integer, charpts integer, maxhp integer, hp integer)
     c.execute('SELECT * FROM players WHERE id=?',[plid])
     if not (c.fetchone() is None):
         data.commit()
         data.close()
-        return 'У игрока <@{}> уже есть персонаж.'.format(plid)
+        return '<@{}> already has a character.'.format(plid)
     stats = (plid,0,0,0,0,1,250,250)
     c.execute('INSERT INTO players VALUES (?,?,?,?,?,?,?,?)', stats)
     data.commit()
     data.close()
-    return 'Персонаж игрока <@{}> создан.'.format(plid)
+    return '<@{}>\' character has been created.'.format(plid)
 
 def getstats(plid):
     data = sqlite3.connect('database.db')
     c = data.cursor()
     c.execute('SELECT * FROM players WHERE id=?', [plid])
     stats = c.fetchone()
+    if stats is None:
+        return '<@{}> doesn\'t have a character.'.format(plid)
     data.commit()
     data.close()
     stats = """```markdown
-LVL         {}
-STR         {}
-DEX         {}
-INT         {}
-Stat Points {}
-HP          {}/{}```""".format(stats[1],stats[2],stats[3],stats[4],stats[5],stats[7],stats[6])
+LVL         {0[1]}
+STR         {0[2]}
+DEX         {0[3]}
+INT         {0[4]}
+Stat Points {0[5]}
+HP          {0[7]}/{0[6]}```""".format(stats)
+    return stats
+
+def putpoint(plid,stat,num=1):
+    for i in range(0,len(statList)):
+        if stat in statList[i]:
+            stat = i
+            break
+    if isinstance(stat,str):
+        return 'Wrong stat name! Must be one of:\n`{0!s}`'.format(statList)
+    data = sqlite3.connect('database.db')
+    c = data.cursor()
+    c.execute('SELECT {}, charpts FROM players WHERE id=?'.format(statList[stat]), [plid])
+    stats = list(c.fetchone())
+    stats[0]+=num
+    stats[1]-=num
+    if stats[1] < 0:
+        return 'Not enough points!'
+    c.execute('''UPDATE players
+    SET {} = ?, charpts = ?
+WHERE id = ?'''.format(statList[stat]),(stats[0],stats[1],plid))
+    data.commit()
+    data.close()
+    upstat = list(statList)
+    for i in range(0,len(upstat)):
+        upstat[i] = upstat[i][:3].upper()
+    stats = """```markdown
+{0}         {1[0]} (+{2})
+Stat Points {1[1]} (-{2})```""".format(upstat[stat],stats,num)
     return stats
