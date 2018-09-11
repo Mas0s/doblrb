ddd311e2b87426bf8dd476db746101f8e6247e8e
CreativeeBlackWolf <32525436+CreativeeBlackWolf@users.noreply.github.com>
2018-09-11 20:01:32 +0300

    fix

--- modules/service.py
+++ modules/service.py
@@ -3,6 +3,7 @@ import sqlite3
 def checkchar(plid): # to check the presence of the character in the bot code
     data = sqlite3.connect('database.db')
     с = data.cursor()
+    c.execute('SELECT * FROM players WHERE id=?', [plid])
     if c.fetchone() is None:
         return False
     data.commit()
